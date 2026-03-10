4f9b3cf52adc3bf82f8f6f108255c86345184de7
Suryanshu Nabheet <suryanshunab@gmail.com>
2026-03-10 14:27:21 +0530

    feat(codex): add storage keys and shared settings types

---
+++ src/vs/workbench/contrib/codex/common/storageKeys.ts
@@ -0,0 +1,23 @@
+/*--------------------------------------------------------------------------------------
+ *  Copyright 2026 Suryanshu Nabheet All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt for more information.
+ *--------------------------------------------------------------------------------------*/
+
+// past values:
+// 'codex.settingsServiceStorage'
+// 'codex.settingsServiceStorageI' // 1.0.2
+
+// 1.0.3
+export const CODEX_SETTINGS_STORAGE_KEY = 'codex.settingsServiceStorageII'
+
+
+// past values:
+// 'codex.chatThreadStorage'
+// 'codex.chatThreadStorageI' // 1.0.2
+
+// 1.0.3
+export const THREAD_STORAGE_KEY = 'codex.chatThreadStorageII'
+
+
+
+export const OPT_OUT_KEY = 'codex.app.optOutAll'
