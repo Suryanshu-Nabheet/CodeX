4599a3ac714b8837199d9e96054dadcbf2df483c
Suryanshu Nabheet <suryanshunab@gmail.com>
2026-03-14 14:44:59 +0530

    feat(codex): add React settings and model dropdown

---
+++ src/vs/workbench/contrib/codex/browser/react/src/codex-settings-tsx/index.tsx
@@ -0,0 +1,11 @@
+/*--------------------------------------------------------------------------------------
+ *  Copyright 2026 Suryanshu Nabheet All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt for more information.
+ *--------------------------------------------------------------------------------------*/
+
+import { mountFnGenerator } from '../util/mountFnGenerator.js'
+import { Settings } from './Settings.js'
+
+export const mountCodexSettings = mountFnGenerator(Settings)
+
+
