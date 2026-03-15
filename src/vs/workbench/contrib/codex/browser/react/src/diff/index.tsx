33a1ea28b5adbb80961c907d03431cf2e517fa40
Suryanshu Nabheet <suryanshunab@gmail.com>
2026-03-15 10:40:10 +0530

    feat(codex): add markdown renderers and apply-block controls

---
+++ src/vs/workbench/contrib/codex/browser/react/src/diff/index.tsx
@@ -0,0 +1,8 @@
+/*--------------------------------------------------------------------------------------
+ *  Copyright 2026 Suryanshu Nabheet All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt for more information.
+ *--------------------------------------------------------------------------------------*/
+
+import { diffLines, Change } from 'diff';
+
+export { diffLines, Change }
